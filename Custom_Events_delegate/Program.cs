2058d9646b288c869b9e8becb6e91e5779083ebc
dcomm <dcomm@LANGLAB-29>
2024-02-27 11:35:40 +0530

    Add project files.

---
+++ Custom_Events_delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System ;
+using System.Collections.Specialized;
+
+namespace events {
+    public delegate string mydel(string s);
+    public class Program {
+        event mydel myevent;
+        public string wel(string s)
+        {
+            return " \t Welocme  " + s;
+        }
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.myevent = new mydel(p.wel);
+            String Ans = p.myevent("Aditya Ghodke" + " \n\n\n \t Events Fired ");
+            Console.WriteLine(Ans);
+        }
+    }
+}
